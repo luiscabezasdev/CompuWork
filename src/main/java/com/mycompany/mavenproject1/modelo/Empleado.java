1826cac853a57fceb31f35778d529e89c9b8570b
cabezasdev <luiscabezas25@hotmail.com>
2026-03-15 15:46:39 -0500

    incluye get y set en Empleado

--- src/main/java/com/mycompany/mavenproject1/modelo/Empleado.java
+++ src/main/java/com/mycompany/mavenproject1/modelo/Empleado.java
@@ -11,43 +11,73 @@ import java.time.LocalDate;
  * @author USUARIO
  */
 public class Empleado {
-    
+
     private String nombre;
     private String cedula;
     private LocalDate fechaDeIngreso;
     private double salario;
     private String idEmpleado;
-    
+    private Departamento departamento;
+
     public Empleado(String nombre) {
         this.nombre = nombre;
     }
-    
+
     public Empleado(String nombre, String cedula, LocalDate fechaDeIngreso, double salario, String idEmpleado) {
-        this.nombre = nombre; 
+        this.nombre = nombre;
         this.cedula = cedula;
         this.fechaDeIngreso = fechaDeIngreso;
         this.salario = salario;
         this.idEmpleado = idEmpleado;
-   }
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public LocalDate getFechaDeIngreso() {
+        return fechaDeIngreso;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
     
-   public String getNombre() {
-       return nombre;
-   }
-   
-   public String getCedula(){
-       return cedula;
-   }
-   
-   public LocalDate getFechaDeIngreso(){
-       return fechaDeIngreso;
-   }
-   
-   public double getSalario() {
-       return salario;
-   }
-   
-   public String getIdEmpleado() {
-       return idEmpleado;
-   }
-       
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void getCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+   public void setFechaDeIngreso(LocalDate fechaDeIngreso) {
+        this.fechaDeIngreso = fechaDeIngreso;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    } 
+
 }
