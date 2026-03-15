c9533faac943a9a71cbd5ff2c3fdcd383a1a800b
cabezasdev <luiscabezas25@hotmail.com>
2026-03-15 16:38:54 -0500

    Nuevas instancias

--- src/main/java/com/mycompany/mavenproject1/servicio/EmpleadoPermanente.java
+++ src/main/java/com/mycompany/mavenproject1/servicio/EmpleadoPermanente.java
@@ -3,12 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mavenproject1.servicio;
-package com.mycompany.mavenproject1.modelo.Empleado;
+import com.mycompany.mavenproject1.modelo.Empleado;
+import java.time.LocalDate;
 
 /**
  *
  * @author USUARIO
  */
 public class EmpleadoPermanente extends Empleado {
+    private double bono;
+
+    public EmpleadoPermanente(String nombre, String cedula, LocalDate fechaDeIngreso, double salario, String idEmpleado, double bono) {
+        super(nombre, cedula, fechaDeIngreso, salario, idEmpleado);
+        this.bono = bono;
+    }
     
+    public double getBono() {
+        return bono;
+    }
+    
+    public void setBono(double bono) {
+        this.bono = bono;
+    }
+  
 }
