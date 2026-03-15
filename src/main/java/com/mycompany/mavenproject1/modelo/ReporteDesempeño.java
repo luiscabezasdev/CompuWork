42152b87c9d26560a6f23c83391127662db30a4a
orlandoosorio-art <orlando.osorio@est.iudigital.edu.co>
2026-03-15 16:58:40 -0500

    Refactor ReporteDesempeño to Reporte class

--- src/main/java/com/mycompany/mavenproject1/modelo/ReporteDesempeño.java
+++ src/main/java/com/mycompany/mavenproject1/modelo/ReporteDesempeño.java
@@ -8,6 +8,52 @@ package com.mycompany.mavenproject1.modelo;
  *
  * @author USUARIO
  */
-public class ReporteDesempeño {
-    
+package com.mycompany.mavenproject1.modelo;
+
+public class Reporte {
+
+    private int idReporte;
+    private String tipo;
+    private String formato;
+
+    public Reporte() {
+    }
+
+    public Reporte(int idReporte, String tipo, String formato) {
+        this.idReporte = idReporte;
+        this.tipo = tipo;
+        this.formato = formato;
+    }
+
+    public int getIdReporte() {
+        return idReporte;
+    }
+
+    public void setIdReporte(int idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public void generarReporteGeneral() {
+        System.out.println("Generando reporte general de tipo: " + tipo);
+    }
+
+    public void exportar(String formato) {
+        System.out.println("Exportando reporte en formato: " + formato);
+    }
 }
